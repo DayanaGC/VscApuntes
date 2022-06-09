c939ef59d8931111955d35653bf73bb40f9da0ce
DayanaGC <dayanagaytan564@gmail.com>
2022-06-09 14:58:29 -0600

    imp,promesas,fetch agregados

--- src/bases/08-imp-expo.js
+++ src/bases/08-imp-expo.js
@@ -1,25 +1,27 @@
 
 //import { owners } from './data/heroes'
 
-import superHeroes from './data/heroes'
+import superHeroes from '../data/heroes'
 
 
 // getHeroById ( id )
 //funciones de Flecha
 
 
-const getHeroById = ( id ) => superHeroes.find( hero => hero.id === id)
-
-
-console.log( getHeroById(2) ) //Spiderman
+export const getHeroById = ( id ) => superHeroes.find( hero => hero.id === id)
 
 
 
 //getHeroesByOwner 'DC', 'Marverl'
-const getHeroesByOwner = ( owner ) => superHeroes.filter( hero => hero.owner === owner)
-console.log( getHeroesByOwner('Marvel') )
+export const getHeroesByOwner = ( owner ) => superHeroes.filter( hero => hero.owner === owner)
 
 
+//import { getHeroById, getHeroesByOwner } from './bases/08-imp-expo'
+
+
+//console.log( getHeroById(2) ) //Spiderman
+
+//console.log( getHeroesByOwner('Marvel') ) // []
 
 
 
