469cfad0191fc24422015637b236e030780e4971
DayanaGC <dayanagaytan564@gmail.com>
2022-06-14 15:08:33 -0600

    inicio de fundamentos

---
+++ src/components/Counter.vue
@@ -0,0 +1,60 @@
+<template>
+    <h2>Counter!!</h2> 
+    <p> {{ counter}} <sup>2</sup> = {{ squareCounter}}</p>
+
+    <div>
+
+        <button @click="increase">+1</button> 
+        <button @click="decrease">-1</button> 
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    //name: "patito"
+    data() {
+        return {
+            counter: 5
+        }
+    },
+    methods: {
+        getSquareValue() {
+            return this.counter *this.counter
+        },
+        increase() {
+            this.counter++
+        },
+        decrease() {
+            this.counter--
+        },
+    },
+    computed: {
+        squareCounter() {
+        return this.counter * this.counter
+        }
+    }
+}
+</script>
+
+<style>
+
+button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid black;
+    color: black;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
+
+</style>
+
